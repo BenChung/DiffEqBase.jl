2fe90d703eb6400cdb31afcf6f17e885c46d2439
Benjamin Chung <ckfinite@gmail.com>
2024-10-30 13:23:36 -0700

    Add reinitialization_method for change_t_via_interpolation to let the use site configure the method

--- src/integrator_interface.jl
+++ src/integrator_interface.jl
@@ -308,9 +308,10 @@ function auto_dt_reset!(integrator::DEIntegrator)
 end
 
 """
-    change_t_via_interpolation!(integrator::DEIntegrator,t,modify_save_endpoint=Val{false})
+    change_t_via_interpolation!(integrator::DEIntegrator,t,modify_save_endpoint=Val{false}; reinitialization_method=nothing)
 
-Modifies the current `t` and changes all of the corresponding values using the local interpolation. If the current solution
+Modifies the current `t` and changes all of the corresponding values using the local interpolation, then ensures DAE consistency (if applicable)
+with `reinitialization_method`. If the current solution
 has already been saved, one can provide the optional value `modify_save_endpoint` to also modify the endpoint of `sol` in the
 same manner.
 """
